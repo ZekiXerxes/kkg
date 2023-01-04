0422c3ffc2a766f1fa916d81bdf7c1af310dd8a8
ZekiXerxes <56410159+ZekiXerxes@users.noreply.github.com>
2023-01-04 14:13:15 +0530

    Update footer.vue

--- app/components/partials/footer.vue
+++ app/components/partials/footer.vue
@@ -100,3 +100,13 @@
     },
   };
 </script>
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator';
+@Component
+export default class Footer extends Vue {}
+</script>
+<style lang="scss">
+.footer {
+  background-color: $bluise;
+}
+</style>
